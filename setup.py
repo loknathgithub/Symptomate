b22ccfe9afb23a6337944d3064fa01f9a8d83ef6
Loknath PC <loknathsaha2004@gmail.com>
2025-10-12 15:52:17 +0530

    folder structure created:v2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Symptomate",
+    version="0.0.0",
+    author="Loknath Saha",
+    author_email="loknathsaha2004@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
